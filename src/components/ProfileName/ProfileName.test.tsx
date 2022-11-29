048945742137bd49d593b4246dab8a1d25a356b8
Matheus Rocha <matheus.rocha89@gmail.com>
2022-11-29 19:15:11 +0000

    Add makeFragmentData function

--- src/components/ProfileName/ProfileName.test.tsx
+++ src/components/ProfileName/ProfileName.test.tsx
@@ -1,10 +1,16 @@
+import { makeFragmentData } from "../../gql";
+import { ProfileName_PersonFragmentDoc } from "../../gql/graphql";
 import { render, screen } from "@testing-library/react";
 import { ProfileName } from "./ProfileName";
 
 describe("<ProfileName />", () => {
   it("renders correctly", () => {
     const profile = { name: "Adam" };
-    render(<ProfileName profile={profile} />);
+    render(
+      <ProfileName
+        profile={makeFragmentData(profile, ProfileName_PersonFragmentDoc)}
+      />
+    );
 
     expect(screen.getByText("Person Name: Adam")).toBeInTheDocument();
   });
